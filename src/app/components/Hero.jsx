42e8317700e17840439805d648a148d1f569cb62
Daya3611 <dayanand.gawade@vsit.edu.in>
2024-07-17 21:30:44 +0530

    Resolved merge conflicts

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -59,7 +59,7 @@ function Hero() {
 
             {/* animate-text-gradient sm:inline-flex bg-gradient-to-r from-neutral-900 via-slate-500 to-neutral-500 bg-[200%_auto] bg-clip-text leading-tight text-transparent dark:from-neutral-100 dark:via-slate-400 dark:to-neutral-400 italic */}
                 <h1 className="animate-text-gradient bg-gradient-to-r from-neutral-800 via-slate-400 to-blue-700 bg-[200%_auto] bg-clip-text landing-tight text-transparent dark:from-blue-100 dark:via-blue-400 dark:to-blue-900 italic  text-center font-bold text-3xl md:text-5xl lg:text-6xl  sm:3xl md:4xl "><strong className=''>Dayanand Gawade .</strong></h1>
-                <h2 className="text-md md:text-lg lg:text-2xl text-gray-600 text-end font-medium dark:text-gray-400">A Fullstack Web Developer</h2>
+                <h2 className="text-md md:text-lg lg:text-2xl text-gray-600 text-end font-medium dark:text-gray-400 italic">A Fullstack Web Developer</h2>
             </div>
             <p className="mt-4 text-base md:text-lg lg:text-xl text-gray-800 font-mono dark:text-gray-300">
                 <Typewriter
