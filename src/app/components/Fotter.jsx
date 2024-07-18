444838ff87c6c7b183089518b1c40af5b39c699f
Daya3611 <dayanand.gawade@vsit.edu.in>
2024-07-18 19:56:28 +0530

    update 1

--- src/app/components/Fotter.jsx
+++ src/app/components/Fotter.jsx
@@ -5,7 +5,7 @@ function Fotter() {
     <div>
       {/* <hr /> */}
       <div className='p-7 text-center bg-transparent'>
-        <h3>©2022 <a href="https://github.com/Daya3611" className='italic text-blue-700'>Daya3611</a> all rights reserved.</h3>
+        <h3>©2024 <a href="https://github.com/Daya3611" className='italic text-blue-700'>Daya3611</a> all rights reserved.</h3>
       </div>
     </div>
   )
