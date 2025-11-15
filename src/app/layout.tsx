80d6f57e5bf2aa5ae57d71b279adb4825d09c6ad
Daya3611 <hi@dayanandgawade.in>
2025-11-15 18:23:49 +0530

    bug fix for vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Exo,} from "next/font/google";
+import { Exo } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "next-themes";
