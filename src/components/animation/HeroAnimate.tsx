8d12d3e7b4c6887c71be924c4f494c9b312cc9cc
Daya3611 <hi@dayanandgawade.in>
2025-11-15 13:34:23 +0530

    feat:change ui - hero sec & nav + animation 2.0

--- src/components/animation/HeroAnimate.tsx
+++ src/components/animation/HeroAnimate.tsx
@@ -7,14 +7,14 @@ export function HeroAnimated() {
   return (
     <BackgroundLines className="flex items-center justify-center w-full flex-col px-4 bg-black">
       <h2 className="-m-9 text-xl">Hey, there <span className="animate-accordion-do">👋</span> I'm</h2>
-      <h2 className=" bg-clip-text text-transparent text-center
+      <strong className=" bg-clip-text text-transparent text-center
     bg-gradient-to-b from-white to-neutral-500
     text-6xl sm:text-4xl md:text-6xl lg:text-7xl
     font-sans font-bold tracking-tight
     py-4 sm:py-6 md:py-10
     relative z-20">
         Dayanand <br className="md:hidden " /> Gawade, <br /> <FlipText />
-      </h2>
+      </strong>
 
       <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-100 text-center">
         <EncryptedText
